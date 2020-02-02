92afef94016d842fa8813a0d699786953a6fb7cb
Jacob Sage <46393418+SageJacob@users.noreply.github.com>
2020-02-02 00:17:24 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpvPxAVlFlB-BxY27YpeEKvvNAF4Us_YM',
+  authDomain: 'visabop-29b7a.firebaseapp.com',
+  databaseURL: 'https://visabop-29b7a.firebaseio.com',
+  projectId: 'visabop-29b7a',
+  storageBucket: 'visabop-29b7a.appspot.com',
+  messagingSenderId: '69314345106',
+  appId: '1:69314345106:web:572c9edf6a7b311198ce4b',
+  measurementId: 'G-BLM0C8F147'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { database };
